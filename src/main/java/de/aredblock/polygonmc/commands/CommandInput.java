bec23ba8f8bcbd34d58257ed0fef9728709c4289
aredblock <aredblockyt@gmail.com>
2024-06-29 16:32:11 +0200

    Player implementation in the CommandInput.class has been added

--- src/main/java/de/aredblock/polygonmc/commands/CommandInput.java
+++ src/main/java/de/aredblock/polygonmc/commands/CommandInput.java
@@ -2,6 +2,7 @@ package de.aredblock.polygonmc.commands;
 
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.entity.Player;
 
 public final class CommandInput {
 
@@ -11,6 +12,14 @@ public final class CommandInput {
     public CommandInput(CommandSender sender, CommandContext context) {
         this.sender = sender;
         this.context = context;
+    }
+
+    public Player getPlayer() {
+        return isFromPlayer() ? (Player) sender : null;
+    }
+
+    public boolean isFromPlayer(){
+        return sender instanceof Player;
     }
 
     public CommandSender getSender() {
